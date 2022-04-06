b48cd181b9685ad0f1993647ba3fdd08691b0d20
fitme <fitme96@gmail.com>
2022-04-06 18:21:01 +0800

    添加钉钉镜像

---
+++ dingtalk/build.sh
@@ -0,0 +1,2 @@
+sudo docker build --build-arg DINGTALK_URL=https://dtapp-pub.dingtalk.com/dingtalk-desktop/xc_dingtalk_update/linux_deb/Release/com.alibabainc.dingtalk_1.3.0.20214_amd64.deb --build-arg DINGTALK_VERSION=1.3.0.20214 -t dingtalk:1.3.0.20214 .
+
